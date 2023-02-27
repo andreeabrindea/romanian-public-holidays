183715a9d5e468d1588388bdf98a8ef41c0f309c
andreeabrindea <79668619+andreeabrindea@users.noreply.github.com>
2023-02-27 03:05:44 +0200

    needs further testing

--- run_test.go
+++ run_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"errors"
+	"github.com/google/go-cmp/cmp"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestGetResponseBody(t *testing.T) {
@@ -33,4 +35,52 @@ func TestGetResponseBody(t *testing.T) {
 			t.Errorf("got error %v, expected nil", err)
 		}
 	})
+}
+func TestConvertTOJSONWithDate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		data     []byte
+		expected []time.Time
+	}{
+		{
+			name: "valid response",
+			data: []byte(`
+				{
+					"response": {
+						"holidays": [
+							{
+								"date": {
+									"iso": "2023-03-01"
+								}
+							},
+							{
+								"date": {
+									"iso": "2023-04-01"
+								}
+							}
+						]
+					}
+				}`),
+			expected: []time.Time{
+				time.Date(2023, 3, 1, 0, 0, 0, 0, time.UTC),
+				time.Date(2023, 4, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+		{
+			name: "invalid json",
+			data: []byte(`
+				{
+					"response": {
+				}`),
+			expected: []time.Time{},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, _ := ConvertTOJSONWithDate(tc.data)
+			if diff := cmp.Diff(got, tc.expected); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
 }
